4335558cbe2e58f22924f60b00f3e717dcfadd4c
mp1707 <marco.preuss1707@gmail.com>
2025-07-25 12:40:11 +0200

    unused files

--- src/features/image-capture/ui/ImageCaptureButton.styles.ts
+++
@@ -1,8 +0,0 @@
-import { StyleSheet } from 'react-native';
-import { spacing } from '../../../theme';
-
-export const styles = StyleSheet.create({
-  button: {
-    // No positioning - handled by parent container
-  },
-});
