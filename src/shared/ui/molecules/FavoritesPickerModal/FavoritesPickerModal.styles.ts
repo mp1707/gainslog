7a5a620279ef1dfe9dc941faf35a032c21f7c07d
mp1707 <marco.preuss1707@gmail.com>
2025-08-12 11:44:11 +0200

    new icons

--- src/shared/ui/molecules/FavoritesPickerModal/FavoritesPickerModal.styles.ts
+++ src/shared/ui/molecules/FavoritesPickerModal/FavoritesPickerModal.styles.ts
@@ -17,10 +17,6 @@ export const createStyles = (colors: any, theme: any) =>
       justifyContent: "space-between",
       paddingVertical: theme.spacing.md,
     },
-    searchBarContainer: {
-      paddingHorizontal: theme.spacing.pageMargins.horizontal,
-      paddingBottom: theme.spacing.md,
-    },
     closeText: {
       color: colors.accent,
     },
