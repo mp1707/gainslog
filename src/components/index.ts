011e3df67a2694e941d1db5c5831ccd0d57df5eb
mp1707 <marco.preuss1707@gmail.com>
2025-08-13 18:54:47 +0200

    icon experiments

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,6 +5,7 @@ export { FilterBadge } from "./FilterBadge";
 export { ProgressRow } from "./ProgressRow";
 export { SemanticBadge } from "./SemanticBadge";
 export { SkeletonShimmerEffect } from "./SkeletonShimmerEffect";
+export { AppIcon } from "./AppIcon";
 
 // Re-export shared Button atom to centralize imports
 export { Button } from "@/shared/ui/atoms/Button";
