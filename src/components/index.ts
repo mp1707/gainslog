9f19b515a46998cd31c76e0f82180b52c7c31f34
mp1707 <marco.preuss1707@gmail.com>
2025-09-13 12:17:29 +0200

    cleanup

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,6 @@
 // Export all design system components
 export { AppText } from "./shared/AppText";
 export { Card } from "./Card";
-export { FilterBadge } from "./FilterBadge";
-export { SemanticBadge } from "./SemanticBadge";
 export { AppIcon } from "./AppIcon";
 
 // Re-export shared Button atom to centralize imports
